ee837972dc2c42971feb8036739b539de73e01c1
Ulysses1013 <kanaina99102@gmail.com>
2021-09-13 18:26:15 +0900

    add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { FaBars } from 'react-icons/fa'
-
+import logo from '../assets/mylogo.svg'
 import Links from '../constants/links'
 
 const Navbar = ({ toggle }) => {
@@ -10,7 +10,7 @@ const Navbar = ({ toggle }) => {
       <div className="nav-center">
         <div className="nav-header" id="top">
           <Link to="/" className="nav-title">
-            MecaBlog
+            <img src={logo} alt="navbar-logo" className="nav-logo"/>
           </Link>
           <button aria-label="toggle" className="toggle-btn" onClick={toggle} >
             <FaBars></FaBars>
